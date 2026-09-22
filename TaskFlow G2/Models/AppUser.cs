cf6fab0cc4a5980ec0abae5a654463bf84ceafee
jzavago98 <jzavago@gmail.com>
2026-09-21 20:19:52 -0600

    Added User and Task Models

---
+++ TaskFlow G2/Models/AppUser.cs
@@ -0,0 +1,10 @@
+namespace TaskFlow_G2.Models;
+
+public class AppUser
+{
+    public string Id { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
