bbfa3c23411307363244d4a77cb3fecefa535785
Jose Cruz <j.d.c.dc.98@unitec.edu>
2026-09-21 20:10:24 -0600

    Models

---
+++ Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DefaultNamespace;
+
+public class User
+{
+    public string FullName { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
+    
+    public string PasswordHash { get; set; } = string.Empty;
+}
